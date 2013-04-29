32e223a959789fcd381b1d6ee84154d417fd6f2a
François Kooman <fkooman@tuxed.net>
2013-04-29 10:41:12 +0200

    use type and value comparison

--- www/js/askAuthorization.js
+++ www/js/askAuthorization.js
@@ -16,7 +16,7 @@
 function setupToggle() {
     document.getElementById('showDetails').addEventListener("click", function () {
         var dT = document.getElementById("detailsTable");
-        if (dT.style.display == "block") {
+        if (dT.style.display === "block") {
             dT.style.display = "none";
         } else {
             dT.style.display = "block";
