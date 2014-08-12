d893f45dda103a4b289fd203b89a25780d19f0c9
François Kooman <fkooman@tuxed.net>
2014-08-12 21:23:45 +0200

    implement initDatabase script

--- bin/initDatabase.php
+++ bin/initDatabase.php
@@ -3,11 +3,8 @@
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
 
 use fkooman\Config\Config;
-
 use fkooman\OAuth\Server\PdoOAuthStorage;
 
 $config = Config::fromIniFile(dirname(__DIR__) . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "oauth.ini");
-
 $storage = new PdoOAuthStorage($config);
-$sql = file_get_contents('schema/db.sql');
-$storage->dbQuery($sql);
+$storage->initDatabase();
